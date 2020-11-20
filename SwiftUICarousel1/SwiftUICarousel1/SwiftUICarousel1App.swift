de80f862f25401858c72b3b9037d0f4854f940ae
Andreea Andro <andreea.andro@fortech.ro>
2020-11-20 10:05:41 +0200

    Added empty project

---
+++ SwiftUICarousel1/SwiftUICarousel1/SwiftUICarousel1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICarousel1App.swift
+//  SwiftUICarousel1
+//
+//  Created by Andreea Andro on 20/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICarousel1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
